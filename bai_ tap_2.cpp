d71b9550464792510de9071b2daca8c7c9e83633
tienvovn <tienxinhtrai886@gmail.com>
2023-09-15 22:01:28 +0700

    Commit message

---
+++ bai_ tap_2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std ;
+int main () {
+    float  r;
+    double chu_vi = 2 * 3.14 * r;
+    double Dien_tich = 3.14 * r * r;
+    cout<< "nhap r:";
+    cin >> r ;
+    cout << "chu vi HT = " << chu_vi << endl ;
+    cout << "dien_tich HT =" << Dien_tich;
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0 ;
+
+}
